5969c63891cfe4c5142c57ae146ad7a681ad8b23
yassine.fadhlaoui <yassine.fadhlaoui@t-systems.com>
2023-10-22 14:55:06 +0200

    format message

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ while true
 do
     stress_time=$(( ( RANDOM % 5 )  + 1 ))
     memory_mbytes=$(( ( RANDOM % 50 )  + 1 ))
-    echo "Starting stress process with the following: stress time: $stress_time, 1 cpu stressor, 1 ram stressor using $memory_mbytes MB of memory"
+    echo -e "\e[34m\e[1mStarting stress process with the following:\e[0m\n\e[1m* Stress time:\e[0m \e[7m$stress_time seconds\e[0m\n\e[1m* CPU stressor: \e[0m\e[7m 1\e[0m\n\e[1m* RAM stressor: \e[0m\e[7m1 using $memory_mbytes MB of memory\e[0m"
     stress-ng --cpu 1 --vm 1 --vm-bytes "$memory_mbytes"m --timeout "$stress_time"s --metrics-brief
     sleep_time=$(( ( RANDOM % 5 )  + 1 ))
     echo "sleeping for $sleep_time"
